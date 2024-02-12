08bf010ba4900e9169ccef17dd339bf7f2c1c704
kikiou91 <gadorjani@windowslive.com>
2024-02-12 18:29:21 +0000

    created mapping through the recentJobs array to display the jobs

--- src/components/JobCard/index.jsx
+++ src/components/JobCard/index.jsx
@@ -1,7 +1,7 @@
 function JobCard({ salary, type, title, company, logo, skills }) {
   return (
     <div className='flex py-5 px-2 justify-between items-start bg-zinc-700 odd:bg-zinc-500'>
-      <div className='flex gap-1 items-start'>
+      <div className='flex gap-1 items-start' style={{ minWidth: '350px' }}>
         <img className='h-20 w-20' src={logo} />
         <div className='flex flex-col'>
           <p className='text-2xl text-white'>{title}</p>
@@ -12,7 +12,7 @@ function JobCard({ salary, type, title, company, logo, skills }) {
         <p className='text-sm text-white'>{type}</p>
       </button>
       <p className='text-base text-white'>{`£${salary}`}</p>
-      <div className='bg-teal-400 rounded-xl p-1 mr-10'>
+      <div className='bg-teal-400 rounded-xl p-1 mr-16'>
         <p className='text-sm text-white'>{skills}</p>
       </div>
     </div>
--- src/components/RecentJobs/index.jsx
+++ src/components/RecentJobs/index.jsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import JobCard from '../JobCard';
+import { JobContext } from '../../context';
 
 function RecentJobs() {
+  const { recentJobs } = useContext(JobContext);
+  console.log(recentJobs);
   return (
-    <section className='font-sans items-center justify-center mx-auto overflow-hidden px-10 max-w-screen-md' style={{ minWidth: '750px' }}>
-      <div className='flex flex-row justify-center py-3'>
+    <section className='font-sans items-center justify-center mx-auto overflow-hidden px-10 max-w-screen-md' style={{ minWidth: '950px' }}>
+      <div className='flex flex-row justify-center items-center py-3'>
         <h3 className='flex justify-start font-bold text-3xl'>Most Recent Jobs</h3>
         <a className='flex ml-auto font-semibold text-blue-600' href='#'>
           View all jobs &#8594;
@@ -13,8 +17,8 @@ function RecentJobs() {
       <div>
         <table className='w-full bg-zinc-700'>
           <thead>
-            <tr className='flex justify-between px-2 mr-10'>
-              <th>Job Title / Company</th>
+            <tr className='flex justify-between px-3 mr-16'>
+              <th style={{ minWidth: '350px' }}>Job Title / Company</th>
               <th className='ml-5 '>Type</th>
               <th className='ml-5'>Salary</th>
               <th>Skills</th>
@@ -22,11 +26,11 @@ function RecentJobs() {
           </thead>
 
           <tbody>
-            <tr className='flex flex-col' style={{ width: '100%' }}>
-              <JobCard salary={`${5000}`} type={'Full-time'} title={'Soft Dev'} company={'Google'} logo={'/src/assets/Red_square.png'} skills={'PHP'} />
-              <JobCard salary={`${5000}`} type={'Full-time'} title={'Soft Dev'} company={'Google'} logo={'/src/assets/Red_square.png'} skills={'PHP'} />
-              <JobCard salary={`${5000}`} type={'Full-time'} title={'Soft Dev'} company={'Google'} logo={'/src/assets/Red_square.png'} skills={'PHP'} />
-            </tr>
+            {recentJobs.map((job) => (
+              <tr key={job.id} className='flex flex-col' style={{ width: '100%' }}>
+                <JobCard salary={job.salary != null ? job.salary : ' -'} type={job.type != null ? job.type : 'N/A'} title={job.job_title} company={job.company} logo={job.logo} skills={'php'} />
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>

0aa2009699704cac897fae7249bf6a5d1188a4f8
kikiou91 <gadorjani@windowslive.com>
2024-02-12 12:26:15 +0000

    Update index.jsx

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,15 +1,9 @@
 'use client';
 import { createContext } from 'react';
 
-const JobContext = createContext(null > null);
+const JobContext = createContext();
 
 const JobContextProvider = ({ children }) => {
-  //function to fetch recent jobs
-
-  //function to fetch all jobs
-
-  //useEffect to run it once
-
   const contextValue = {};
   return <JobContext.Provider value={contextValue}>{children}</JobContext.Provider>;
 };

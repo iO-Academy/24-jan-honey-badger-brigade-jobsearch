322429ef75a0f9b91a844cb7defc0ef4af5d75a6
Emma Kim <eema.kim01@gmail.com>
2024-02-12 15:34:39 +0000

    add basic outline and placeholder in NavMenu

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -1,5 +1,22 @@
+import { NavLink } from "react-router-dom"
+import HomePage from "../HomePage"
+
 function NavMenu() {
-    return ()
+    return (
+        <>
+            <nav className="">
+                <a href="#">JobSearch</a>
+                <span>
+                    <a href="#">All Jobs</a>
+                    <a href="#">Full time</a>
+                    <a href="#">Part time</a>
+                    <a href="#">Contract</a>
+                </span>
+                <a href="#">Post a Job</a>
+
+            </nav>
+        </>
+    )
 
 }
 

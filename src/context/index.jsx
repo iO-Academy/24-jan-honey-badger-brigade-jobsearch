8df77573bb4159af00949499f0c3ff5d7de83b54
kikiou91 <gadorjani@windowslive.com>
2024-02-13 16:45:26 +0000

    update to search api call

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,10 +1,27 @@
 'use client';
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 const JobContext = createContext();
 
 const JobContextProvider = ({ children }) => {
-  const contextValue = {};
+  const [jobs, setJobs] = useState([]);
+  const [search, setSearch] = useState('developer');
+
+  const fetchJobs = async () => {
+    const res = await fetch(`https://job-search-api.dev.io-academy.uk/jobs?search=${search}`);
+    const data = await res.json();
+    setJobs(data);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  console.log(jobs);
+  const contextValue = {
+    jobs,
+    setJobs,
+  };
   return <JobContext.Provider value={contextValue}>{children}</JobContext.Provider>;
 };
 

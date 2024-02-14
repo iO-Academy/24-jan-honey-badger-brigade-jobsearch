5405f0b91c3dcc196a45a1c759d642a54f74fea1
kikiou91 <gadorjani@windowslive.com>
2024-02-14 16:29:58 +0000

    finished story three

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -31,7 +31,7 @@ function JobModal() {
   }
 
   return (
-    <section onClick={toggleModal} className='p-2.5 z-10 bg-zinc-500/50 w-full min-h-screen fixed overflow-auto top-0 left-0'>
+    <section className='p-2.5 z-10 bg-zinc-500/50 w-full min-h-screen fixed overflow-auto top-0 left-0'>
       <div className='Modal-container px-4 m-2 md:mx-auto md:my-2 w-full md:max-w-screen-md overflow-auto z-20 absolute my-5 bg-white left-1/2 transform -translate-x-1/2'>
         <div className='flex justify-between my-4 ml-2 border-slate-200 border-b-2 pb-4'>
           <div className='flex flex-row gap-2 font-bold'>
@@ -39,7 +39,7 @@ function JobModal() {
             <h2>{jobCompany}</h2>
           </div>
           <span>
-            <svg onClick={toggleModal} xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='3' stroke='currentColor' className='w-6 h-6 stroke-blue-500'>
+            <svg onClick={toggleModal} xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='3' stroke='currentColor' className='cursor-pointer w-6 h-6 stroke-blue-500'>
               <path strokeLinecap='round' strokeLinejoin='round' d='M6 18 18 6M6 6l12 12' />
             </svg>
           </span>

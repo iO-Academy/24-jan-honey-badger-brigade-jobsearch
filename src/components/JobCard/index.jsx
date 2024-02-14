47d28f87bf5b8d001abe71a980fba5a37db43fc6
Jenny <jenny@redsparkdigital.co.uk>
2024-02-14 12:54:55 +0000

    mobile view and code fixes

--- src/components/JobCard/index.jsx
+++ src/components/JobCard/index.jsx
@@ -1,19 +1,25 @@
-function JobCard({ salary, type, title, company, logo, skills }) {
-  return (
-    <div className='flex py-5 px-2 justify-between items-start'>
-      <div className='flex gap-1 items-start' style={{ minWidth: '350px' }}>
-        <img className='h-20 w-20' src={logo} />
-        <div className='flex flex-col'>
-          <p className='text-2xl text-white'>{title}</p>
-          <p className='text-xl text-white'>{company}</p>
+import { useContext, useState } from "react";
+import modalContext from "../../context/modalContext";
+
+function JobCard({ salary, type, title, company, logo, skills, url }) {
+  const {modal, toggleModal} = useContext(modalContext)
+  
+  return ( 
+    <div className='flex flex-col even:bg-zinc-700 odd:bg-zinc-500 w-full'  
+    id={url} onClick={toggleModal}>
+      <div className='flex py-5 px-2 justify-between items-start'>
+        <div className='flex gap-2 w-[45%] pr-2'>
+          <img className='h-20 w-20 hidden md:block' src={logo} />
+          <div className='flex flex-col'>
+            <p className='text-2xl text-white'>{title}</p>
+            <p className='text-xl text-white'>{company}</p>
+          </div>
         </div>
-      </div>
-      <button className='bg-blue-500 rounded-xl p-1 '>
-        <p className='text-sm text-white'>{type}</p>
-      </button>
-      <p className='text-base text-white'>{`£${salary}`}</p>
-      <div className='bg-teal-400 rounded-xl p-1 mr-16'>
-        <p className='text-sm text-white'>{skills}</p>
+        <div className='w-[15%]'>
+          <span className='font-semibold text-sm text-white px-1.5 py-0.5 bg-blue-500 rounded-lg'>{type}</span>
+        </div>
+        <p className='text-base text-white w-[15%]'>{`£${salary}`}</p>
+        <span className='flex gap-2 items-center justify-start w-[25%] flex-wrap'>{skills}</span>
       </div>
     </div>
   );
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,9 +1,34 @@
 import { useContext } from 'react';
 import { JobContext } from '../../context';
+import modalContext from '../../context/modalContext';
 
-const JobModal = ({ toggle }) => {
-  const { recentJobs } = useContext(JobContext);
-  const job = recentJobs[0];
+function JobModal() {
+  const {modalID, toggleModal} = useContext(modalContext)
+  const [jobTitle, setTitle] = useState('')
+  const [jobCompany, setCompany] = useState('')
+  const [jobType, setType] = useState('')
+  const [jobImg, setImg] = useState('')
+  const [jobSalary, setSalary] = useState('')
+  const [jobDate, setDate] = useState('')
+  const [jobSkills, setSkills] = useState([])
+  const [jobDesc, setDesc] = useState('')
+  const jobURL = `https://job-search-api.dev.io-academy.uk/jobs/${modalID}`
+  useEffect(getJobDetails, [])
+
+  function getJobDetails() {
+    fetch(jobURL)
+    .then(res => res.json())
+    .then (data => {
+      setTitle(data.job_title)
+      setCompany(data.company)
+      setType(data.type != null ? data.type : 'N/A')
+      setImg(data.logo)
+      setDesc(data.job_description)
+      setSalary(data.salary != null ? data.salary : ' -')
+      setDate(data.posted)
+      setSkills(data.skills)
+    })
+  }
 
   return (
     <div className={`Modal-container px-2 border-2 border-slate-400 z-10 fixed my-5 bg-white  left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2`}>
@@ -12,74 +37,64 @@ const JobModal = ({ toggle }) => {
           <h2>{`${job.job_title} -`}</h2>
           <h2>{job.company}</h2>
         </div>
-        <button onClick={toggle} className='ml-auto'>
-          X
-        </button>
-      </div>
-      <div className='logo-company text-white bg-blue-500 flex flex-row gap-3 items-center pl-5 mx-2' style={{ height: '100px' }}>
-        <h1 className='font-semibold text-lg'>Junior Software Engineer</h1>
-        <h2 className='bg-green-600 rounded-full p-1'>Job type</h2>
-      </div>
-      <div className='mx-10 mt-5'>
-        <div className='key-details'>
-          <div className='flex flex-row gap-2 items-center'>
-            <img src={job.logo} alt='company logo' width={65} />
-            <h2 className='font-semibold'>{job.company}</h2>
+        <div className='bg-zinc-50'>
+          <div className='logo-company text-white bg-blue-500 flex flex-row gap-3 items-center pl-5 py-10'>
+            <h1 className='font-semibold text-xl'>{jobTitle}</h1>
+            <h2 className='bg-green-600 rounded-full px-1.5 py-0.5 font-bold text-xl'>{jobType}</h2>
           </div>
-          <div className='font-semibold'>Key facts: </div>
-          <div className='facts flex flex-row gap-12'>
-            <div className='flex flex-col gap-1'>
-              <div>Salary:</div>
-              <div>{`$${job.salary}`}</div>
-            </div>
-            <div className='flex flex-col gap-1'>
-              <div>Type: </div>
-              <div>Full time</div>
-            </div>
-            <div className='flex flex-col gap-1'>
-              <div>Data posted: </div>
-              <div>{'2024.05.25'}</div>
-            </div>
-            <div className='flex flex-col gap-1'>
-              <div>Skills</div>
-              <div className='flex flex-row gap-1'>
-                {job.skills.map((skill) => (
-                  <span key={skill.id} className='bg-blue-500 text-white p-1 rounded-lg'>
-                    {skill.skill}
-                  </span>
-                ))}
+          <div className='bg-white m-4 p-1'>
+            <div className='key-details m-5'>
+              <div className='flex flex-row gap-2 items-center'>
+                <img src={jobImg} alt='company logo' width={65} />
+                <h2 className='font-bold text-xl'>{jobCompany}</h2>
               </div>
-            </div>
-          </div>
-        </div>
-        <div className='description mt-2'>
-          <h2 className='font-bold'>Job description</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum quod velit modi voluptatum quibusdam quis iure, iste quia atque sint mollitia nostrum eos fugit! Recusandae nemo doloremque
-            nam non illo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum quod velit modi voluptatum quibusdam quis iure, iste quia atque sint mollitia nostrum eos fugit! Recusandae
-            nemo doloremque nam non illo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum quod velit modi voluptatum quibusdam quis iure, iste quia atque sint mollitia nostrum eos
-            fugit! Recusandae nemo doloremque nam non illo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum quod velit modi voluptatum quibusdam quis iure, iste quia atque sint
-            mollitia nostrum eos fugit! Recusandae nemo doloremque nam non illo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum quod velit modi voluptatum quibusdam quis iure, iste
-            quia atque sint mollitia nostrum eos fugit! Recusandae nemo doloremque nam non illo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum quod velit modi voluptatum quibusdam
-            quis iure, iste quia atque sint mollitia nostrum eos fugit! Recusandae nemo doloremque nam non illo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum quod velit modi
-            voluptatum quibusdam quis iure, iste quia atque sint mollitia nostrum eos fugit! Recusandae nemo doloremque nam non illo.
-          </p>
-        </div>
-        <div className='flex mt-6 items-center'>
-          <button className='bg-green-600 text-white p-2 rounded-lg'>Apply Now</button>
-          <button className='ml-auto rounded-lg border-2 p-1 border-zinc-400 text-zinc-400'>Save for later</button>
-        </div>
-      </div>
-      <div className='mt-8 mx-10'>
-        <h1 className='text-4xl font-bold mb-4'>Similar Jobs</h1>
-      </div>
-      <div className='flex flex-row border w-1/4 mx-10 bg-zinc-500 mb-4'>
-        <div className='flex gap-2 items-center p-2'>
-          <img src={job.logo} alt='company logo' width={100} />
-        </div>
-        <div className='flex flex-col pt-2 text-white w-1/2 '>
-          <h2 className='font-bold text-l mb-2'>{job.job_title}</h2>
-          <h2 className='font-bold text-l'>{job.company}</h2>
+              <div className='font-bold text-xl'>Key facts: </div>
+                <div className='facts flex flex-row gap-2'>
+                  <div className='flex flex-col gap-1 basis-1/4 md:basis-2/12'>
+                    <div className='font-bold'>Salary:</div>
+                    <div>{`£${jobSalary}`}</div>
+                  </div>
+                  <div className='flex flex-col gap-1 basis-1/4 md:basis-2/12'>
+                    <div className='font-bold'>Type: </div>
+                    <div>{jobType}</div>
+                  </div>
+                  <div className='flex flex-col gap-1 basis-1/4 md:basis-2/12'>
+                    <div className='font-bold'>Data posted: </div>
+                    <div>{jobDate}</div>
+                  </div>
+                  <div className='flex flex-col gap-1 basis-1/4 md:basis-6/12'>
+                    <div className='font-bold'>Skills</div>
+                      <div className='flex flex-row gap-1 flex-wrap'>
+                        {jobSkills.map((skill) => (
+                        <span key={skill.id} className='bg-blue-500 text-white p-1 rounded-lg'>
+                          {skill.skill}
+                        </span>
+                      ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='description m-2 p-1'>
+                  <h2 className='font-bold'>Job description</h2>
+                  <p>{jobDesc}</p>
+                </div>
+                <div className='flex my-6 mx-2 p-1 items-center'>
+                  <button className='bg-green-600 text-white p-2 rounded-lg'>Apply Now</button>
+                  <button className='ml-auto rounded-lg border-2 p-1 border-zinc-400 text-zinc-400'>Save for later</button>
+                </div>
+              </div>
+          {/* <div className='mt-8 mx-10'>
+                <h1 className='text-4xl font-bold mb-4'>Similar Jobs</h1>
+              </div>
+              <div className='flex flex-row border w-1/4 mx-10 bg-zinc-500 mb-4'>
+                <div className='flex gap-2 items-center p-2'>
+                  <img src={jobImg} alt='company logo' width={100} />
+                </div>
+                <div className='flex flex-col pt-2 text-white w-1/2 '>
+                <h2 className='font-bold text-l mb-2'>{jobTitle}</h2>
+                <h2 className='font-bold text-l'>{jobCompany}</h2>
+                </div> 
+              </div>  */}
         </div>
       </div>
     </div>
